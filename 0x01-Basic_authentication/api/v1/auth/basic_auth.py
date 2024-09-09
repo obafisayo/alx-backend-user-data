5311d58b38b937d36ee269c32cd6875641c96c4f
obafisayo <sekaniisjoy@gmail.com>
2024-09-09 23:44:23 +0100

    updated tha basic auth with init

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,8 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ creates a basic authorization for the user"""
-    pass
+
+    def __init__(self) -> None:
+        """initialize auth class"""
+        super().__init__()
+        pass
