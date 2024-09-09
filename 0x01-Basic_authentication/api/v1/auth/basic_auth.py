3172b4420e46478d147b36a00816766b4e029024
obafisayo <sekaniisjoy@gmail.com>
2024-09-10 00:02:20 +0100

    added function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,14 @@ class BasicAuth(Auth):
         """initialize auth class"""
         super().__init__()
         pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ extract base64 authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.split()[0] is not "Basic":
+            return None
+        return authorization_header.split()[1] 
